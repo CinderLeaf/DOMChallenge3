88dfe3200a655a03e4bec4733937be4badca386c
Yimni Chan <ynchan818@gmail.com>
2020-06-02 00:48:51 -0400

    saved functions

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 let rows = 1;
 let columns = 3;
 
+//
+let colored = false;
+
 //grab the table
 let tbl = document.getElementById("myTable");
 
 //#1: add rows to the grid
 function appendRow() {
-    let newRow = tbl.insertRow(-1);
+   
+    let newRow = document.createElement("tr");
     for(let i = 0; i < columns; i++) {
-        let  newCell = newRow.insertCell(-1);
+        let  newCell = document.createElement("td");
+        setProperties(newCell);
+        newRow.appendChild(newCell);
     }
+    tbl.appendChild(newRow);
     rows++;
 }
 
@@ -18,7 +25,10 @@ function appendRow() {
 function appendColumn() {
     let selectAllRows = document.querySelectorAll("tr");
     for(let i = 0; i < rows; i++) {
-        selectAllRows[i].insertCell(-1);
+        //selectAllRows[i].insertCell(-1);
+        let  newCell = document.createElement("td");
+        setProperties(newCell);
+        selectAllRows[i].appendChild(newCell);
     }
     columns++;
 }
@@ -38,4 +48,33 @@ function deleteColumn() {
     columns--;
 }
 
+/*-----------------------------------------------*/
+function selectedColor(color) {
+    currentColor = color;
+}
 
+function setProperties(cell) {
+    // #5 select a color from a dropdown menu of colors
+    // #6 click on a single cell, changing its color to the currently selected color
+    cell.classList.add("noColor");
+    cell.addEventListener("click", changeColor);
+
+    cell.addEventListener("mousedown", color => {
+        colored = true;
+    });
+    cell.addEventListener("mousemove", color => {
+      if (colored) {
+        changeColor(cell)
+      }
+    });
+    cell.addEventListener("mouseup", color => {
+      if (colored) {
+        colored = false;
+      }
+    });
+}
+
+function changeColor(){
+    this.style.backgroundColor = currentColor;
+    this.classList.remove("noColor");
+}
