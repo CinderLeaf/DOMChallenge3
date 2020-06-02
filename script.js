371a0e94388b0da6a0a7fcaba8711180c4b5d67f
Yimni Chan <ynchan818@gmail.com>
2020-06-02 02:27:30 -0400

    add functions

--- script.js
+++ script.js
@@ -8,6 +8,50 @@ let colored = false;
 //grab the table
 let tbl = document.getElementById("myTable");
 
+// all cells set it's properties
+let tblCells = document.getElementsByTagName("td");
+let tblCellsArray = [...tblCells];
+//event handlers and sets class 
+for (let i=0; i < tblCellsArray.length; i++) {
+    const cell = tblCellsArray[i];
+    setProperties(cell);
+}
+
+function setProperties(cell) {
+
+    // #6 click on a single cell, changing its color to the currently selected color
+    cell.classList.add("noColor");
+    cell.addEventListener("click", changeColor);
+    
+    //#10: click and hold (mouseover) from a single cell (start) to a different cell (end)
+    // such that all affected/hovered-over cells from start to end change to the currently selected color.
+    cell.addEventListener("mousedown", color => {
+        colored = true;
+    });
+    cell.addEventListener("mousemove", color => {
+      if (colored) {
+        changeColor(cell)
+      }
+    });
+    cell.addEventListener("mouseup", color => {
+      if (colored) {
+        colored = false;
+      }
+    });
+}
+
+let currentColor;
+
+function selectedColor(color) {
+    currentColor = color;
+}
+//
+function changeColor(){
+    this.style.backgroundColor = currentColor;
+    this.classList.remove("noColor");
+}
+
+
 //#1: add rows to the grid
 function appendRow() {
    
@@ -48,33 +92,25 @@ function deleteColumn() {
     columns--;
 }
 
-/*-----------------------------------------------*/
-function selectedColor(color) {
-    currentColor = color;
+/**
+ * // all cells set it's properties
+let tblCells = document.getElementsByTagName("td");
+let tblCellsArray = [...tblCells];
+ */
+
+// #7: fill all uncolored cells with the currently selected color -->
+function fillNonColored(){
+ 
+    
 }
+    
+// #8: fill all cells with the currently selected color -->
+function fillAllColor(){
 
-function setProperties(cell) {
-    // #5 select a color from a dropdown menu of colors
-    // #6 click on a single cell, changing its color to the currently selected color
-    cell.classList.add("noColor");
-    cell.addEventListener("click", changeColor);
 
-    cell.addEventListener("mousedown", color => {
-        colored = true;
-    });
-    cell.addEventListener("mousemove", color => {
-      if (colored) {
-        changeColor(cell)
-      }
-    });
-    cell.addEventListener("mouseup", color => {
-      if (colored) {
-        colored = false;
-      }
-    });
 }
+// #9: clear all cells/restore all cells to their original/initial color -->
+function clearAll(){
 
-function changeColor(){
-    this.style.backgroundColor = currentColor;
-    this.classList.remove("noColor");
+
 }
