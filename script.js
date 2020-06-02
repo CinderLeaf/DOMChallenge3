623821bf9493a1712edb2b06d1036d02e781ec4c
Yimni Chan <ynchan818@gmail.com>
2020-06-01 21:31:58 -0400

    saved

--- script.js
+++ script.js
@@ -1,55 +1,40 @@
-/*
-function createCell(cell, text, style) {
-    let div = document.createElement('div');
-    let txt = document.createTextNode(text); 
+//the exist row and column number
+let rows = 1;
+let columns = 3;
 
-    div.appendChild(txt);                    
-    div.setAttribute('class', style);        
-    div.setAttribute('className', style);    
-    cell.appendChild(div);                   
- }*/
- var amountofRows = 1;
- var amountofColumns = 2;
+//grab the table
+let tbl = document.getElementById("myTable");
 
-// #1: add rows to the grid
+//#1: add rows to the grid
 function appendRow() {
-
-    let tbl = document.getElementById('myTable');
-    let newrow = document.createElement("tr");
-
-    for (let i = 0; i < 2; i++) {
-        newrow.insertCell(i);
+    let newRow = tbl.insertRow(-1);
+    for(let i = 0; i < columns; i++) {
+        let  newCell = newRow.insertCell(i);
     }
+    //tbl.appendChild(newRow);
+    rows++;
 }
 
 //#2: add columns to the grid
 function appendColumn() {
-    let tbl = document.getElementById('myTable');
-    let row = tbl.insertRow(tbl.rows.length);
-
-    for (leti = 0; i < tbl.rows.length; i++) {
-        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
+    let selectAllRows = document.querySelectorAll("tr");
+    for(let i = 0; i < rows; i++) {
+        selectAllRows[i].insertCell(-1);
     }
+    columns++;
 }
 
-// #3: remove rows from the grid 
+/* Feature #3: remove rows from the grid */
 function deleteRow() {
-    let tbl = document.getElementById('myTable');
-    let lastRow = tbl.rows.length - 1;             
-
-    for (let i = lastRow; i > 0; i--) {
-        tbl.deleteRow(i);
-    }
+    tbl.deleteRow(-1);
+    rows--;
 }
- 
-// #4: remove columns from the grid
-function deleteColumn() {
-    let tbl = document.getElementById('myTable');
-    let lastCol = tbl.rows[0].cells.length - 1;
 
-    for (let i = 0; i < tbl.rows.length; i++) {
-        for (let j = lastCol; j > 0; j--) {
-            tbl.rows[i].deleteCell(j);
-        }
+/* Feature #4: remove columns from the grid */
+function deleteColumn() {
+    let selectAllRows = document.querySelectorAll("tr");
+    for(let i = 0; i < rows; i++) {
+        selectAllRows[i].deleteCell(-1);
     }
+    columns--;
 }
