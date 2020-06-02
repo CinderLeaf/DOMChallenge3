64bd0d9b610c1e6265be537fe63bad41bbb813f8
Yimni Chan <ynchan818@gmail.com>
2020-06-01 20:02:03 -0400

    change variable declaring

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // #1: add rows to the grid
 function createCell(cell, text, style) {
-    var div = document.createElement('div'), 
-        txt = document.createTextNode(text); 
+    let div = document.createElement('div');
+    let txt = document.createTextNode(text); 
 
     div.appendChild(txt);                    
     div.setAttribute('class', style);        
@@ -11,44 +11,40 @@ function createCell(cell, text, style) {
 
 // #1: add rows to the grid
 function appendRow() {
-    var tbl = document.getElementById('myTable'),
-        row = tbl.insertRow(tbl.rows.length),
-        i;     
+    let tbl = document.getElementById('myTable');
+    let row = tbl.insertRow(tbl.rows.length);
 
-    for (i = 0; i < tbl.rows[0].cells.length; i++) {
+    for (let i = 0; i < tbl.rows[0].cells.length; i++) {
         createCell(row.insertCell(i), i, 'row');
     }
 }
 
 //#2: add columns to the grid
 function appendColumn() {
-    var tbl = document.getElementById('myTable'),
-        i;
+    let tbl = document.getElementById('myTable');
 
-    for (i = 0; i < tbl.rows.length; i++) {
+    for (leti = 0; i < tbl.rows.length; i++) {
         createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
     }
 }
 
 // #3: remove rows from the grid 
 function deleteRow() {
-    var tbl = document.getElementById('myTable'), 
-        lastRow = tbl.rows.length - 1,             
-        i;
+    let tbl = document.getElementById('myTable');
+    let   lastRow = tbl.rows.length - 1;             
 
-    for (i = lastRow; i > 0; i--) {
+    for (let i = lastRow; i > 0; i--) {
         tbl.deleteRow(i);
     }
 }
  
 // #4: remove columns from the grid
 function deleteColumn() {
-    var tbl = document.getElementById('myTable'),
-        lastCol = tbl.rows[0].cells.length - 1,   
-        i, j;
+    let tbl = document.getElementById('myTable');
+    let lastCol = tbl.rows[0].cells.length - 1;
 
-    for (i = 0; i < tbl.rows.length; i++) {
-        for (j = lastCol; j > 0; j--) {
+    for (let i = 0; i < tbl.rows.length; i++) {
+        for (let j = lastCol; j > 0; j--) {
             tbl.rows[i].deleteCell(j);
         }
     }
